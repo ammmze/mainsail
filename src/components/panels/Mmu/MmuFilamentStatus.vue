f6c929c6db75582597b33b9520c84b3bebd35b4a
moggieuk <32878385+moggieuk@users.noreply.github.com>
2025-01-22 02:31:54 +0700

    MmuFilamentStatus.vue

---
+++ src/components/panels/Mmu/MmuFilamentStatus.vue
@@ -0,0 +1,237 @@
+<template>
+<svg xmlns="http://www.w3.org/2000/svg"
+     xmlns:xlink="http://www.w3.org/1999/xlink"
+     width="50%" height="50%" viewBox="180 15 275 405" fill="none"
+     preserveAspectRatio="xMidYMid meet" ref="filStatusSvg">
+  <defs>
+    <g id="sync-feedback" :style="'stroke:' + colorOutline + '; stroke-linecap: round; stroke-linejoin: round; fill: none;'">
+      <path d="M18,9,13.78,3.39a1,1,0,0,0-1.56,0L8,9" style="stroke-width: 1; stroke-opacity: 0.8"></path>
+      <path d="M13,8.24,18,15H15H8Z" style="stroke-width: 2;"></path>
+    </g>
+    <g id="sissors" :style="'stroke:' + colorOutline + ';fill: none; stroke-linecap: round; stroke-linejoin: round;'">
+      <path d="M8.8,7.72c-.6,1.21-2.34,1.64-3.89,1S2.6,6.48,3.2,5.28s2.34-1.64,3.89-1S9.4,6.52,8.8,7.72Zm-3.89,1L21,16M7.09,19.68c-1.55.68-3.29.25-3.89-1s.17-2.73,1.71-3.4,3.29-.25,3.89,1S8.63,19,7.09,19.68ZM21,8,4.91,15.32" style="fill: none; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path>
+      <line x1="31" y1="12.5" x2="44" y2="12.5" style="stroke-width: 1; stroke-dasharray: 2,2;" />
+    </g>
+  </defs>
+
+  <rect x="150" y="30" width="265" height="130" fill="#808080" fill-opacity="0.1" rx="10" ry="10"/>
+  <rect x="150" y="333" width="265" height="66" fill="#808080" fill-opacity="0.1" rx="10" ry="10"/>
+  <g :style="'stroke:' + colorOutline + '; stroke-linecap: round; stroke-linejoin: round; stroke-width: 1;'">
+    <path d="M242 25 L242 405 L249 411 L251 411 L258 405 L258 25" style="fill: none; stroke-linecap: round; stroke-linejoin: round; stroke-width: 1;"></path>
+  </g>
+
+  <g> /* PAUL make this is single dynamic line (and a cut fragment) */
+    <rect id="f_pregate" x="243" y="25" width="14" height="25" fill="#8CDFAC" />
+    <rect id="f_gear" x="243" y="50" width="14" height="30" fill="#8CDFAC" />
+    <rect id="f_gate" x="243" y="80" width="14" height="30" fill="#8CDFAC" />
+    <rect id="f_encoder" x="243" y="110" width="14" height="30" fill="#8CDFAC" />
+    <rect id="f_bowden_start" x="243" y="140" width="14" height="20" fill="#8CDFAC" />
+    <rect id="f_extruder_entry" x="243" y="160" width="14" height="160" fill="#8CDFAC" />
+    <rect id="f_extruder" x="243" y="320" width="14" height="12" fill="none" />
+    <rect id="f_toolhead" x="243" y="332" width="14" height="18" fill="none" />
+    <rect id="f_cut" x="243" y="350" width="14" height="30" fill="none" />
+    <polygon id="f_nozzle" points="257,380 243,380 243,405 249,412 249,413 251,413 251,412 257,405" fill="purple" />
+  </g>
+
+  <g :style="'stroke:' + colorOutline + '; fill:' + colorFont + '; stroke-linecap: round; stroke-linejoin: round; stroke-width: 0; font-family: Roboto; font-size: 16;'">
+    <g v-if="hasPregateSensor()">
+      <circle cx="258" cy="50" r="8" :style="'fill:' + pregateSensorColor + '; stroke-width:1; stroke-dasharray:0'" />
+      <text x="278" y="55">Pre-Gate</text>
+    </g>
+
+    <g v-if="hasGearSensor()">
+      <circle cx="258" cy="80" r="8" :style="'fill:' + gearSensorColor + '; stroke-width:1; stroke-dasharray:0'" />
+      <text x="278" y="85">Gear</text>
+      <text v-if="homedToGear()" x="219.5" y="85" font-weight="bold">H</text>
+    </g>
+
+    <g v-if="hasGateSensor()">
+      <circle cx="258" cy="110" r="8" :style="'fill:' + gateSensorColor + '; stroke-width:1; stroke-dasharray:0'" />
+      <text x="278" y="115">Gate</text>
+      <text v-if="homedToGate()" x="219.5" y="115" font-weight="bold">H</text>
+    </g>
+  
+    <circle cx="258" cy="140" r="8" :style="'fill:' + encoderSensorColor + '; stroke-width:1; stroke-dasharray:0'" />
+    <path d="M257 135 L261 140 L257 145" stroke-width="2" fill="none" />
+    <text x="278" y="145">Encoder: {{ encoderPos }}mm</text>
+    <text v-if="homedToEncoder()" x="219.5" y="145" font-weight="bold">H</text>
+  
+    <circle cx="258" cy="320" r="8" :style="'fill:' + extruderSensorColor + '; stroke-width:1; stroke-dasharray:0'" />
+    <text x="278" y="325">Extruder</text>
+    <text v-if="homedToExtruder()" x="219.5" y="325" font-weight="bold">H</text>
+
+    <circle cx="258" cy="350" r="8" :style="'fill:' + toolheadSensorColor + '; stroke-width:1; stroke-dasharray:0'" />
+    <text x="278" y="355">Toolhead</text>
+    <text v-if="homedToToolhead()" x="219.5" y="355" font-weight="bold">H</text>
+
+    <g v-if="hasSyncFeedback()">
+      <text x="278" y="240">Sync-Feedback</text>
+      <g v-if="syncCompressionTriggered()">
+        <use xlink:href="#sync-feedback" transform="translate(210, 200) scale(1.2)"/>
+        <use xlink:href="#sync-feedback" transform="translate(210, 270) scale(1.2,-1.2)"/>
+      </g>
+      <g v-if="syncTensionTriggered()">
+        <use xlink:href="#sync-feedback" transform="translate(210, 235) scale(1.2)"/>
+        <use xlink:href="#sync-feedback" transform="translate(210, 235) scale(1.2,-1.2)"/>
+      </g>
+    </g>
+  </g>
+
+  <use xlink:href="#sissors" transform="translate(205, 365) scale(1.2)"/>
+</svg>
+</template>
+
+<script lang="ts">
+/* PAUL: simplier Component ... import Component from 'vue-class-component' */
+import { Component, Mixins, Watch, Prop, Emit } from 'vue-property-decorator'
+import BaseMixin from '@/components/mixins/base'
+
+@Component({})
+export default class MmuFilamentStatus extends Mixins(BaseMixin) {
+/*
+    @Prop({ type: Boolean, required: false, default: true }) has_pregate_sensor: boolean | null
+    @Prop({ required: false, default: mdiChevronDown }) declare readonly iconExpanded: string | null
+*/
+
+/* PAUL: Example of watch.. encoderPos = number of characters typed */
+    encoderData: string = '';
+
+    get encoderPos(): number {
+        return this.encoderData.length;
+    }
+
+    @Watch('username')
+    onEncoderDataChanged(newVal: string, oldVal: string) {
+        console.log(`Username changed from ${oldVal} to ${newVal}`);
+    }
+
+/* PAUL: Parent could listen with...
+    @Emit('date-sent') // PAUL: date-sent is the event name
+    sendDate() {
+        console.log("PAUL: Sending new date")
+        return new Date();  // This date will be sent as the payload of the event
+    }
+  <div>
+    <ChildComponent @date-sent="handleDateReceived"/>
+  </div>
+
+    handleDateReceived(date: Date) {
+        console.log("Received date: ", date);
+    }
+*/
+
+    // PAUL: Fixme for light theme
+    private primaryBackground = "#1E1E1E"
+    private highlightedBackground = "#272727"
+    private testing = false
+
+/*
+    data() {
+      return {
+        encoderPos: "433mm"
+      };
+    }
+*/
+
+/* PAUL: Vue lifecycle hooks (created, mounted, updated, ... */
+    mounted() {
+      console.log("PAUL: mounted()")
+    }
+
+    get colorOutline(): string {
+        return '#2CA9BC'
+    }
+    get colorOutlineContrast(): string {
+        return '#222222'
+    }
+    get colorFont(): string {
+        return '#FFFFFF'
+    }
+    get pregateSensorColor(): string {
+        if (this.testing) {
+            return this.highlightedBackground
+        } else {
+            return 'limegreen'
+        }
+    }
+    get gearSensorColor(): string {
+        if (this.testing) {
+            return this.highlightedBackground
+        } else {
+            return 'limegreen'
+        }
+    }
+    get gateSensorColor(): string {
+        if (this.testing) {
+            return this.highlightedBackground
+        } else {
+            return 'limegreen'
+        }
+    }
+    get encoderSensorColor(): string {
+        if (this.testing) {
+            return this.highlightedBackground
+        } else {
+            return 'limegreen'
+        }
+    }
+    get extruderSensorColor(): string {
+        if (this.testing) {
+            return this.primaryBackground
+        } else {
+            return 'limegreen'
+        }
+    }
+    get toolheadSensorColor(): string {
+        if (!this.testing) {
+            return this.highlightedBackground
+        } else {
+            return 'limegreen'
+        }
+    }
+
+    hasPregateSensor(): boolean {
+        return true
+        /* return this.has_pregate_sensor */
+    }
+    hasGearSensor(): boolean {
+        return true
+        /* return this.has_gear_sensor */
+    }
+    hasGateSensor(): boolean {
+        return true
+    }
+    hasSyncFeedback(): boolean {
+        return true
+    }
+
+    syncCompressionTriggered(): boolean | null {
+        return false
+    }
+    syncTensionTriggered(): boolean | null {
+        return true
+    }
+
+    homedToPreGate(): boolean {
+        return false
+    }
+    homedToEncoder(): boolean {
+        return false
+    }
+    homedToGear(): boolean {
+        return false
+    }
+    homedToGate(): boolean {
+        return false
+    }
+    homedToExtruder(): boolean {
+        return true
+    }
+    homedToToolhead(): boolean {
+        return false
+    }
+}
+</script>
+
+<style scoped>
+</style>
